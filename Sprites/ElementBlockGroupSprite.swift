04f30c41e3221343ef1ae4dd68670a28d40b80da
lewisbriffa <lewis.a.briffa@gmail.com>
2018-04-30 21:51:25 +0100

    Refactor sprite creation

---
+++ Sprites/ElementBlockGroupSprite.swift
@@ -0,0 +1,9 @@
+//
+//  ElementBlockGroupingSprite.swift
+//  ElementalSpace
+//
+//  Created by Lewis Briffa on 30/04/2018.
+//  Copyright © 2018 Lewis Briffa. All rights reserved.
+//
+
+import Foundation
